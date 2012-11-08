0f0477fe506b9e03571aaeaddd237243bb191ee2
mbl111 <matt@mbl111.com>
2012-11-08 17:20:54 +1100

    Basic menu system

---
+++ GitHubGameJam2012/src/com/mbl111/ggo12/gfx/menu/Menu.java
@@ -0,0 +1,26 @@
+package com.mbl111.ggo12.gfx.menu;
+
+import com.mbl111.ggo12.Game;
+import com.mbl111.ggo12.gfx.Screen;
+
+public class Menu {
+	
+	public Game game;
+
+	public void init(Game game){
+		this.game = game;
+	}
+	
+	public void tick() {
+
+	}
+
+	public boolean isFullScreen() {
+		return false;
+	}
+
+	public void render(Screen screen) {
+		
+	}
+
+}
---
+++ GitHubGameJam2012/src/com/mbl111/ggo12/gfx/menu/MenuStack.java
@@ -0,0 +1,53 @@
+package com.mbl111.ggo12.gfx.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mbl111.ggo12.Game;
+import com.mbl111.ggo12.gfx.Screen;
+
+public class MenuStack {
+
+	public List<Menu> menus = new ArrayList<Menu>();
+	public Game game;
+
+	public MenuStack(Game game) {
+		this.game = game;
+	}
+
+	public void addMenu(Menu menu) {
+		menu.init(game);
+		this.menus.add(menu);
+	}
+
+	public void removeTopMenu() {
+		if (menus.size() > 0) menus.remove(menus.size() - 1);
+	}
+
+	public void removeMenu(Menu menu) {
+		menus.remove(menu);
+	}
+
+	public void tick() {
+		int i = menus.size() - 1;
+		Menu m = menus.get(i);
+		m.tick();
+		while (!m.isFullScreen()) {
+			i--;
+			m = menus.get(i);
+			m.tick();
+		}
+	}
+
+	public void render(Screen screen) {
+		int i = menus.size() - 1;
+		Menu m = menus.get(i);
+		m.render(screen);
+		while (!m.isFullScreen()) {
+			i--;
+			m = menus.get(i);
+			m.render(screen);
+		}
+	}
+
+}
