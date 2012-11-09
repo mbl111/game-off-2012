a55abf9dfe5ee65955597c9fb8f2e95f92e60fb1
mbl111 <matt@mbl111.com>
2012-11-09 12:19:20 +1100

    Added Tile WIDTH and HEIGHT

--- GitHubGameJam2012/src/com/mbl111/ggo12/level/tile/Tile.java
+++ GitHubGameJam2012/src/com/mbl111/ggo12/level/tile/Tile.java
@@ -2,11 +2,13 @@ package com.mbl111.ggo12.level.tile;
 
 public class Tile {
 
+	public static final int WIDTH = 16;
+	public static final int HEIGHT = 16;
 	public static Tile[] tiles = new Tile[256];
-	
-	public static Tile byid(int id){
+
+	public static Tile byid(int id) {
 		if (id > tiles.length) return null;
 		return tiles[id];
 	}
-	
+
 }
