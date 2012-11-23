c4b7a3e42a3b4d7084624694552dd01fd30d0e59
mbl111 <matt@mbl111.com>
2012-11-23 15:55:15 +1100

    Removed unused imports from the tile class

--- GitHubGameJam2012/src/com/mbl111/ggo12/level/tile/GrassTile.java
+++ GitHubGameJam2012/src/com/mbl111/ggo12/level/tile/GrassTile.java
@@ -1,10 +1,6 @@
 package com.mbl111.ggo12.level.tile;
 
 import com.mbl111.ggo12.Util.SyncRandom;
-import com.mbl111.ggo12.entity.Entity;
-import com.mbl111.ggo12.gfx.Art;
-import com.mbl111.ggo12.gfx.Screen;
-import com.mbl111.ggo12.level.Level;
 
 public class GrassTile extends Tile {
 
